9c31cae2590535a7bdbede57d69139e3d9a00232
qzr <18278000811@163.com>
2018-11-28 21:08:43 +0800

    刚开始

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// 登录请求
+app.post('/api/login',function(req,res){
+
+});
+// 注册请求
+app.post('/api/register',function(req,res){
+
+});
+
+app.post('/api/user/list',function(req,res){
+
+});
